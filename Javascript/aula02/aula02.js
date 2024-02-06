76c1856a1c62c55fd1b1a8a734f342d1ccef7135
Mailson Rodrigues <mailsonrodriguesarts@gmail.com>
2024-02-06 20:01:29 -0300

    Aula Escopo

---
+++ Javascript/aula02/aula02.js
@@ -0,0 +1,17 @@
+var nome = prompt("Olá, digite seu nome")
+document.write(`Seja bem vindo ${nome}`)
+
+console.log(nome)
+
+var valor = prompt("Informe um valor numérico")
+var valorConvertido = parseFloat(valor)
+var valorFloat = 1.3
+
+var soma = valorConvertido + valorFloat
+
+console.log(soma)
+
+var nome01 = prompt("Informe seu nome")
+var nome02 = prompt("Informe seu sobrenome")
+
+alert(`${nome01} ${nome02}`)
